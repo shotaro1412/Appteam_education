30567b6f75913b137071eb4173c7c4c9109e06e6
shotaro <okita0510sb@i.softbank.jpgit config --global user.email okita0510sb@i.softbank.jp>
2024-03-31 15:22:30 +0900

    機能としては完成

--- stopwatch/lib/main.dart
+++ stopwatch/lib/main.dart
@@ -1,101 +1,106 @@
-import 'package:flutter/material.dart';
-import 'dart:async';
-import 'package:provider/provider.dart';
-
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: StopwatchApp(),
-    );
-  }
-}
-
-class StopwatchModel extends ChangeNotifier {
-  bool _isRunning = false;
-  double _counter = 0.0;
-  late Timer _timer;
-
-  bool get isRunning => _isRunning;
-  double get counter => _counter;
-
-  void startTimer() {
-    _timer = Timer.periodic(Duration(milliseconds: 100), (Timer t) {
-      _counter += 0.1;
-      notifyListeners(); 
-    });
-    _isRunning = true;
-    notifyListeners();
-  }
-
-  void stopTimer() {
-    _timer.cancel();
-    _isRunning = false;
-    notifyListeners();
-  }
-
-  void resetTimer() {
-    _counter = 0.0;
-    notifyListeners();
-  }
-}
+// import 'package:flutter/material.dart';
+// import 'dart:async';
+// //setstate版
 
 
+// void main() {
+//   runApp(MyApp());
+// }
 
-class StopwatchApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Stopwatch'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Consumer<StopwatchModel>(
-                builder: (context, model, _) => Text(
-                  '${model.counter.toStringAsFixed(1)}',
-                  style: TextStyle(fontSize: 40.0),
-                ),
-              ),
-              SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      if (!context.read<StopwatchModel>().isRunning) {
-                        context.read<StopwatchModel>().startTimer();
-                      }
-                    },
-                    child: Text('Start'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      if (context.read<StopwatchModel>().isRunning) {
-                        context.read<StopwatchModel>().stopTimer();
-                      }
-                    },
-                    child: Text('Stop'),
-                  ),
-                  ElevatedButton(
-                    onPressed: () {
-                      context.read<StopwatchModel>().resetTimer();
-                    },
-                    child: Text('Reset'),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: StopwatchApp(),
+//     );
+//   }
+// }
+
+// class StopwatchApp extends StatefulWidget {
+//   @override
+//   _StopwatchAppState createState() => _StopwatchAppState();
+// }
+
+// class _StopwatchAppState extends State<StopwatchApp> {
+//   bool isRunning = false;
+//   double counter = 0.0;
+//   late Timer timer;
+
+//   @override
+//   void dispose() {
+//     timer.cancel();
+//     super.dispose();
+//   }
+
+//   void startTimer() {
+//     timer = Timer.periodic(Duration(milliseconds: 100), (Timer t) { 
+//       setState(() {
+//         counter += 0.1; 
+//       });
+//     });
+//   }
+
+//   void stopTimer() {
+//     timer.cancel();
+//   }
+
+//   void resetTimer() {
+//     setState(() {
+//       counter = 0.0;
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Stopwatch'),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Text(
+//               '${counter.toStringAsFixed(1)}',
+//               style: TextStyle(fontSize: 40.0),
+//             ),
+//             SizedBox(height: 20),
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//               children: [
+//                 ElevatedButton(
+//                   onPressed: () {
+//                     if (!isRunning) {
+//                       startTimer();
+//                       setState(() {
+//                         isRunning = true;
+//                       });
+//                     }
+//                   },
+//                   child: Text('Start'),
+//                 ),
+//                 ElevatedButton(
+//                   onPressed: () {
+//                     if (isRunning) {
+//                       stopTimer();
+//                       setState(() {
+//                         isRunning = false;
+//                       });
+//                     }
+//                   },
+//                   child: Text('Stop'),
+//                 ),
+//                 ElevatedButton(
+//                   onPressed: () {
+//                     resetTimer();
+//                   },
+//                   child: Text('Reset'),
+//                 ),
+//               ],
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
